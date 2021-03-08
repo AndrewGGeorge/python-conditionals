1c03b9e89e5c8766d888ac41055985a4ce4c38c2
Andrew George <aggeorge26@gmail.com>
2021-03-08 13:00:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#x = 15
+#y = 7
+
+#if x > y:
+#  print('Yes') 
+
+# ================
+
+#x = 5
+#y = 7
+
+#if x < y:
+#  print('Yes')
+
+# if ('Hello') in ('Hello World!'):
+#    print('yes')
+
+# ================
+
+#x = 5
+#y = 7
+
+#if x > y:
+#  print('Yes it is greater')
+#elif x < y:
+#  print('No it is less')
+#else:
+#  print('Neither')
+
+# ================
+'''
+Nested if statements
+'''
+#num = 3
+
+#if num >= 2:
+ # if num == 2:
+  #  print('Two')
+  #else:
+   #   print('Positive number')
+#else:
+ # print('Negative Number')
+
+ # ================
+'''
+One-line is statements
+'''
+#if 7 > 5: print('Yes it is')
+
+ # ================
+''''
+Ternary operator
+'''
+#All on one line print comes first before the 'if'
+#print('Yes') if 7 > 5 else print('No')
+
+ # ================
+ #if you ar in a middle of something and you have to stop write pass until you are able to get back to writing
+
+if 100 > 7:
+   pass
